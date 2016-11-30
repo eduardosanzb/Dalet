a12bdf47a7f479b213b409d7daef45efc6769c95
eduardosanzb <eduardosanzb@gmail.com>
2016-11-29 21:06:42 -0600

    providers forntend

--- frontend/src/app/pages/providers/provider.tab.controller.js
+++ frontend/src/app/pages/providers/provider.tab.controller.js
@@ -40,14 +40,13 @@
 
         function getCharts(report){
           /* Sorry for this mess, but the xml response is a fucking mess.
-            Mybe I could use something more cool like recursive, but is too late
+            Maybe I could use something more cool like recursive, but is too late
           */
-          
           var lastReport = getLastReport(report.stats)
           vm.platformSearches = []
           vm.platformRequests = []
-          vm.charts = []
           
+          if(!lastReport.content) return console.error('We dont have any reports');
           lastReport.content[0].ItemPerformance.map(function(item){
             item.month = item.Period.Begin.substring(5,7)
             if (item.Category === "Searches"){
@@ -56,31 +55,31 @@
               vm.platformRequests.push(cleanPlatformReport(item))
             }
           })
-
+          
           vm.charts = [].concat([{
                 color: pieColor,
                 description: 'Busquedas específicas',
-                stats: vm.platformSearches[vm.platformSearches.length-2]
+                stats: vm.platformSearches[vm.platformSearches.length-1]
                       .search_reg,
                 icon: 'person',
             },
             {
                 color: pieColor,
                 description: 'Solicitudes',
-                stats: vm.platformRequests[vm.platformSearches.length-2].record_view,
+                stats: vm.platformRequests[vm.platformSearches.length-1].record_view,
                 icon: 'face',
             },
             {
                 color: pieColor,
                 description: 'Busquedas generales',
-                stats: vm.platformSearches[vm.platformSearches.length-2]
+                stats: vm.platformSearches[vm.platformSearches.length-1]
                         .search_fed,
                 icon: 'person',
             },
             {
                 color: pieColor,
                 description: 'Clicks resultados',
-                stats:vm.platformRequests[vm.platformSearches.length-2].result_click,
+                stats:vm.platformRequests[vm.platformSearches.length-1].result_click,
                 icon: 'face',
             }])
 
